364bf7e7ded0ebeeabc14b8456800b2aecdadbb5
Timtohy Eiffe <kaotech@kat.fritz.box>
2021-06-05 13:08:14 +0930

    Updated run.sh with loop

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+RUNNING="0"
 
-./guess.sh
-
-./guess
-
-toilet -f banner --metal WINNER
+while(( RUNNING < 2 ))
+do
+if(( RUNNING == 0 ))
+then
+	./guess.sh;
+	RUNNING=$(( RUNNING+1 ));
+fi
+if(( RUNNING == 1 ))
+then
+	./guess;
+	RUNNING=$(( RUNNING+1 ));
+fi
+done
+	toilet -f banner --metal WINNER
